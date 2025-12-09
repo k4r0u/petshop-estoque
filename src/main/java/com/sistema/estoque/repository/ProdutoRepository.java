c120f32ee7c7f7c5228872c9caf1bbcbd56a868b
kms <karoline_ms@hotmail.com>
2025-12-09 12:16:21 -0300

    Teste de projeto

---
+++ src/main/java/com/sistema/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.sistema.estoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sistema.estoque.model.Produto;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+}
